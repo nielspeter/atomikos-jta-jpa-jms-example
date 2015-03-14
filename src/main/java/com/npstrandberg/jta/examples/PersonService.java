35926045028c005910a9a017aac3c6a9b33a5d97
Niels Peter Strandberg <nielspeter@npstrandberg.com>
2015-03-14 09:43:21 +0100

    renamed

--- src/main/java/com/npstrandberg/jta/examples/PersonService.java
+++ src/main/java/com/npstrandberg/jta/examples/PersonService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class JpaAndJms {
+public class PersonService {
 
     @Autowired
     private JmsTemplate jmsTemplate;
--- src/test/java/com/npstrandberg/jta/examples/PersonServiceTest.java
+++ src/test/java/com/npstrandberg/jta/examples/PersonServiceTest.java
@@ -13,13 +13,13 @@ import static org.junit.Assert.assertNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
-public class JpaAndJmsTest {
+public class PersonServiceTest {
 
     @Autowired
     private PersonRepository personRepository;
 
     @Autowired
-    private JpaAndJms service;
+    private PersonService personService;
 
     @Test
     public void testCommit() throws JMSException {
@@ -29,12 +29,12 @@ public class JpaAndJmsTest {
         person.setName("Niels Peter");
 
         // when
-        service.registerNewPerson(person, false);
+        personService.registerNewPerson(person, false);
 
         // then
         assertEquals(1, personRepository.count());
         assertEquals("Niels Peter", personRepository.findByName("Niels Peter").getName());
-        assertEquals("{name: 'Niels Peter'}", service.receivePersonCreatedMessage());
+        assertEquals("{name: 'Niels Peter'}", personService.receivePersonCreatedMessage());
     }
 
     @Test
@@ -46,13 +46,13 @@ public class JpaAndJmsTest {
 
         // when
         try {
-            service.registerNewPerson(person, true);
+            personService.registerNewPerson(person, true);
         } catch (IllegalStateException e) {
             assertEquals("BOOM", e.getMessage());
         }
 
         // then
         assertEquals(0, personRepository.count());
-        assertNull(service.receivePersonCreatedMessage());
+        assertNull(personService.receivePersonCreatedMessage());
     }
 }
